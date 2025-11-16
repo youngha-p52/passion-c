16d62a1f98f035ca2cdb97cf1a0c3aa28ac6cc60
Youngha Park <youngha.p52@icloud.com>
2025-11-16 22:27:47 +0900

    complete chapter04

---
+++ chapter04/problem04-4/problem1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    printf("정수 입력: ");
+    scanf("%d", &num);
+    num = ~num;
+    num = num + 1;
+    printf("부호를 바꾼 결과: %d \n", num);
+    return 0;
+}
---
+++ chapter04/problem04-4/problem2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num = 3;
+    num = num << 3; // 8의 곱
+    num = num >> 2; // 4의 제
+    printf("%d \n", num);
+    return 0;
+}
