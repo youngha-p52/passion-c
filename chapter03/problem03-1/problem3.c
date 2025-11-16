e29a52f78c43a5dfcd01945745e68a6aa42774f3
Youngha Park <youngha.p52@icloud.com>
2025-11-16 21:40:55 +0900

    complete chapter03

---
+++ chapter03/problem03-1/problem3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    printf("하나의 정수 입력: ");
+    scanf("%d", &num);
+    printf("%d의 제곱의 결과: %d \n", num, num * num);
+    return 0;
+}
---
+++ chapter03/problem03-1/problem4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2;
+    printf("두 개의 정수 입력: ");
+    scanf("%d %d", &num1, &num2);
+    printf("몫: %d, 나머지: %d \n", num1 / num2, num1 % num2);
+    return 0;
+}
